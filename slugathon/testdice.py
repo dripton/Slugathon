7fc645e497a0495f8c1a657d61376e3750a0aebe
David Ripton <dripton@ripton.net>
2003-06-30 03:57:42 +0000

    Add runs, sign, Mann-Kendall tests.

--- slugathon/testdice.py
+++ slugathon/testdice.py
@@ -44,6 +44,44 @@ def countRuns(rolls):
         prev = roll
     return count
 
+def countPositiveDiffs(rolls):
+    prev = 7
+    count = 0
+    for roll in rolls:
+        if roll > prev:
+            count += 1
+        prev = roll
+    return count
+
+def countNonZeroDiffs(rolls):
+    prev = None
+    count = 0
+    for roll in rolls:
+        if prev != None and roll != prev:
+            count += 1
+        prev = roll
+    return count
+
+def trimZeroRuns(rolls):
+    """Return the list with runs of identical rolls reduced to just one."""
+    li = []
+    prev = None
+    for roll in rolls:
+        if roll != prev:
+            li.append(roll)
+        prev = roll
+    return li
+
+def sign(num):
+    """Return 1 if num is positive, 0 if zero, -1 if negative."""
+    if num > 0:
+        return 1
+    elif num == 0:
+        return 0
+    else:
+        return -1
+
+
 def failIfAbnormal(val, mean, var):
     """Fail if a result is outside the normal range."""
     # Avoid division by zero when we hit spot-on.
@@ -56,9 +94,10 @@ def failIfAbnormal(val, mean, var):
     if abs(z) > 3:
         assert False
 
+
 class DiceTestCase(unittest.TestCase):
     def setUp(self):
-        self.trials = 6000
+        self.trials = 600
         self.dice = Dice.Dice()
         self.rolls = []
         self.bins = {}
@@ -82,6 +121,19 @@ class DiceTestCase(unittest.TestCase):
     def testCountRuns(self):
         assert countRuns([1, 2, 2, 2, 3, 2, 1, 6, 6, 5, 5, 1]) == 8
 
+    def testCountPositiveDiffs(self):
+        assert countPositiveDiffs([]) == 0
+        assert countPositiveDiffs([3, 3, 4, 2, 5, 3, 4, 2, 6, 1]) == 4
+
+    def testCountNonZeroDiffs(self):
+        assert countNonZeroDiffs([]) == 0
+        assert countNonZeroDiffs([3, 3, 4, 2, 5, 3, 4, 2, 6, 1]) == 8
+
+    def testTrimZeroRuns(self):
+        assert trimZeroRuns([]) == []
+        assert trimZeroRuns([3, 3, 4, 2, 5, 3, 4, 2, 6, 1]) == \
+                               [3, 4, 2, 5, 3, 4, 2, 6, 1]
+
     def testM(self):
         """Recode each sample as 0 if <= sample median, 1 if > sample median
            M is number of runs of consecutive 0s and 1s.
@@ -103,6 +155,40 @@ class DiceTestCase(unittest.TestCase):
         print "M test: r =", r, "M = ", M, "mean = ", meanM, "var =", varM
         failIfAbnormal(M, meanM, varM)
 
+    def testSign(self):
+        """P is number of positive signs among x2-x1, x3-x2, etc. (not zeros)
+           If m non-zero values of xi - x(i-1), meanP is m/2, varianceP is m/12
+        """
+        P = countPositiveDiffs(self.rolls)
+        m = countNonZeroDiffs(self.rolls)
+        meanP = m / 2.
+        varP = m / 12.
+        print "Sign test: P =", P, "m = ", m, "mean = ", meanP, "var =", varP
+        failIfAbnormal(P, meanP, varP)
+
+    def testRuns(self):
+        trimmed = trimZeroRuns(self.rolls)
+        m = len(trimmed)
+        pos = countPositiveDiffs(trimmed)
+        neg = m - pos
+        R = 0. + pos
+        meanR = 1. + (2 * pos * neg) / (pos + neg)
+        varR = ((( 2. * pos * neg) * (2. * pos * neg - pos - neg)) /
+                ((pos + neg) * (pos + neg) * (pos + neg - 1)))
+        print "Runs test: R =", R, "m = ", m, "mean = ", meanR, "var =", varR
+        failIfAbnormal(R, meanR, varR)
+
+    def testMannKendall(self):
+        S = 0
+        n = len(self.rolls)
+        for i in range(1, n):
+            for j in range(i):
+                val = sign(self.rolls[i] - self.rolls[j])
+                S += val
+        meanS = 0.
+        varS = (n / 18.) * (n - 1.) * (2. * n + 5.)
+        print "Mann-Kendall test: S =", S, "mean = ", meanS, "var =", varS
+        failIfAbnormal(S, meanS, varS)
 
 
 if __name__ == "__main__":
