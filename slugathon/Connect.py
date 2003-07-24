668866c4645cd3b9d57f28a4e4cba993c3d69d0a
David Ripton <dripton@ripton.net>
2003-07-24 04:23:00 +0000

    Rename some widgets for clarity. Start adding default values for player, server, port.

--- slugathon/Connect.py
+++ slugathon/Connect.py
@@ -9,18 +9,53 @@ import gtk
 from gtk import glade
 import sys
 import os
+from sets import Set
+import Server
 
 
 class Connect:
     """GUI for connecting to a server."""
     def __init__(self):
         self.glade = glade.XML('../glade/connect.glade')
-        self.widgets = ['connect', 'chooseServer', 'connectButton', 
+        self.widgets = ['connectWindow', 'playerNameCombo', 'passwordEntry', 
+          'serverNameCombo', 'serverPortCombo', 'connectButton', 
           'startServerButton']
         for widgetName in self.widgets:
             setattr(self, widgetName, self.glade.get_widget(widgetName))
         self.glade.signal_autoconnect(self)
-        self.connect.show()
+        self.playerNames = None
+        self.serverNames = None
+        self.serverPorts = None
+        self.init_lists()
+        self.connectWindow.show()
+
+    def init_lists(self):
+        self.load_prefs()
+        self.init_player_names()
+        self.init_server_names()
+        self.init_server_ports()
+
+    def load_prefs(self):
+        pass
+
+    def save_prefs(self):
+        pass
+
+    def init_player_names(self):
+        if not self.playerNames:
+            self.playerNames = Set()
+        # XXX Not portable
+        self.playerNames.add(os.getenv('USER'))
+
+    def init_server_names(self):
+        if not self.serverNames:
+            self.serverNames = Set()
+        self.serverNames.add('localhost')
+
+    def init_server_ports(self):
+        if not self.serverPorts:
+            self.serverPorts = Set()
+        self.serverPorts.add(Server.DEFAULT_PORT)
 
     def on_connectButton_clicked(self, *args):
         button = args[0]
@@ -38,9 +73,9 @@ def quit(unused):
 
 if __name__ == '__main__':
     connect = Connect()
-    connect.connect.connect("destroy", quit)
+    connect.connectWindow.connect("destroy", quit)
     pixbuf = gtk.gdk.pixbuf_new_from_file('../images/creature/Colossus.gif')
-    connect.connect.set_icon(pixbuf)
+    connect.connectWindow.set_icon(pixbuf)
 
     while 1:
         gtk.mainiteration()
