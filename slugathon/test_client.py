2088cf386e86d01f1a9270392e96b3b55a360b10
David Ripton <dripton@ripton.net>
2003-07-25 13:25:47 +0000

    Skeleton client unit test class.

---
+++ slugathon/test_client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.3
+
+import unittest
+import Client
+import Connect
+
+class ConnectTestCase(unittest.TestCase):
+
+    def testSavePrefs(self):
+        pass
+
+    def testLoadPrefs(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
