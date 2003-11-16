c91b3e3af30c2e867d8757ea7a2ed44223954db1
David Ripton <dripton@ripton.net>
2003-11-16 17:58:09 +0000

    Add some sample values to the models for the list and tree.

--- slugathon/Anteroom.py
+++ slugathon/Anteroom.py
@@ -7,6 +7,7 @@ except ImportError, AttributeError:
     pass
 import gtk
 from gtk import glade
+import gobject
 import sys
 
 
@@ -18,7 +19,29 @@ class Anteroom:
           'playerList']
         for widgetName in self.widgets:
             setattr(self, widgetName, self.glade.get_widget(widgetName))
-        self.anteroom.show()
+
+        self.playerStore = gtk.ListStore(gobject.TYPE_STRING)
+        players = ['dripton', 'tchula']
+        for player in players:
+            it = self.playerStore.append()
+            self.playerStore.set(it, 0, player)
+        self.playerList.set_model(self.playerStore)
+        column = gtk.TreeViewColumn('Player Name', gtk.CellRendererText(),
+          text=0)
+        self.playerList.append_column(column)
+
+        self.gamesStore = gtk.TreeStore(gobject.TYPE_STRING)
+        games = ['Game 1', 'Game 2']
+        for game in games:
+            it = self.gamesStore.append(None)
+            self.gamesStore.set(it, 0, game)
+        self.gamesTree.set_model(self.gamesStore)
+        column = gtk.TreeViewColumn('Game Name', gtk.CellRendererText(),
+          text=0)
+        self.gamesTree.append_column(column)
+
+        self.anteroom.show_all()
+
 
 
 def quit(unused):
