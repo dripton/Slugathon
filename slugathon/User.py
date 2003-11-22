170b151e31c4483078ad64dfa9ee7d49863ab02a
David Ripton <dripton@ripton.net>
2003-11-22 19:18:22 +0000

    Pass server and client into init Add user to server from init Add perspective_getUserNames and perspective_getGames, delegating    to server

--- slugathon/User.py
+++ slugathon/User.py
@@ -5,21 +5,32 @@ from twisted.cred import portal
 class User(pb.Avatar):
     """Perspective for a player or observer."""
 
-    def __init__(self, name):
+    def __init__(self, name, server, client):
         self.name = name
-        self.server = None
+        self.server = server
         self.client = None
+        # XXX Bidirectional references
+        self.server.addUser(self)
+        print "User.init", self, name, server
 
-    def perspective_getname(self, arg):
-        print "perspective_getname(", arg, ") called on", self
+    def perspective_getName(self, arg):
+        print "perspective_getName(", arg, ") called on", self
         return self.name
+
+    def perspective_getUserNames(self):
+        print "perspective_getUserNames called on", self
+        return self.server.getUserNames()
+
+    def perspective_getGames(self):
+        print "perspective_getGames called on", self
+        return self.server.getGames()
+
 
     def __str__(self):
         return "User " + self.name
 
     def attached(self, mind):
         print "called User.attached", mind
-        self.client = mind
         self.client.callRemote("setname", self.name)
         self.client.callRemote("ping", time.time())
 
