bd0adfe79e52577680122fdbf001b73b295afcff
David Ripton <dripton@ripton.net>
2003-11-22 19:22:36 +0000

    Build Anteroom from Client, after connecting to server Move more Anteroom setup into __init__.  Remove the __main__, which    was redundant with the unit test. Have Anteroom fetch usernames and game info from server side. Use list instead of tree to display games.

--- slugathon/Anteroom.py
+++ slugathon/Anteroom.py
@@ -9,49 +9,64 @@ import gtk
 from gtk import glade
 import gobject
 import sys
+import os
+import Server
+import Client
+from twisted.internet import reactor
 
 
 class Anteroom:
     """GUI for a multiplayer chat and game finding lobby."""
-    def __init__(self):
+    def __init__(self, user):
+        self.user = user
         self.glade = glade.XML('../glade/anteroom.glade')
-        self.widgets = ['anteroom', 'chatEntry', 'chatView', 'gamesTree',
-          'playerList']
+        self.widgets = ['anteroomWindow', 'chatEntry', 'chatView', 'gameList',
+          'userList']
         for widgetName in self.widgets:
             setattr(self, widgetName, self.glade.get_widget(widgetName))
+        self.users = []
+        self.games = []
+        self.anteroomWindow.connect("destroy", quit)
+        pixbuf = gtk.gdk.pixbuf_new_from_file(
+          '../images/creature/Colossus.gif')
+        self.anteroomWindow.set_icon(pixbuf)
+        def1 = user.callRemote("getUserNames")
+        def1.addCallbacks(self.gotUserNames, self.failure)
 
-        self.playerStore = gtk.ListStore(gobject.TYPE_STRING)
-        players = ['dripton', 'tchula']
-        for player in players:
-            it = self.playerStore.append()
-            self.playerStore.set(it, 0, player)
-        self.playerList.set_model(self.playerStore)
-        column = gtk.TreeViewColumn('Player Name', gtk.CellRendererText(),
+    def gotUserNames(self, usernames):
+        self.usernames = usernames
+        def1 = self.user.callRemote("getGames")
+        def1.addCallbacks(self.gotGames, self.failure)
+
+    def gotGames(self, games):
+        self.games = games
+
+        self.userStore = gtk.ListStore(gobject.TYPE_STRING)
+        for user in self.users:
+            it = self.userStore.append()
+            self.userStore.set(it, 0, user)
+        self.userList.set_model(self.userStore)
+        column = gtk.TreeViewColumn('User Name', gtk.CellRendererText(),
           text=0)
-        self.playerList.append_column(column)
+        self.userList.append_column(column)
 
-        self.gamesStore = gtk.TreeStore(gobject.TYPE_STRING)
-        games = ['Game 1', 'Game 2']
-        for game in games:
-            it = self.gamesStore.append(None)
-            self.gamesStore.set(it, 0, game)
-        self.gamesTree.set_model(self.gamesStore)
+        self.gameStore = gtk.ListStore(gobject.TYPE_STRING)
+        for game in self.games:
+            it = self.gameStore.append()
+            self.gameStore.set(it, 0, game)
+        self.gameList.set_model(self.gameStore)
         column = gtk.TreeViewColumn('Game Name', gtk.CellRendererText(),
           text=0)
-        self.gamesTree.append_column(column)
+        self.gameList.append_column(column)
 
-        self.anteroom.show_all()
+        self.anteroomWindow.show_all()
 
+
+    def failure(self, error):
+        print "Anteroom.failure", self, error
+        reactor.stop()
 
 
 def quit(unused):
     sys.exit()
 
-if __name__ == '__main__':
-    anteroom = Anteroom()
-    anteroom.anteroom.connect("destroy", quit)
-    pixbuf = gtk.gdk.pixbuf_new_from_file('../images/creature/Colossus.gif')
-    anteroom.anteroom.set_icon(pixbuf)
-
-    while 1:
-        gtk.mainiteration()
--- slugathon/Client.py
+++ slugathon/Client.py
@@ -7,31 +7,42 @@ from twisted.spread import pb
 import Server
 from twisted.cred import credentials
 from twisted.internet import reactor
+import Anteroom
 
 
 class Client(pb.Referenceable):
     def __init__(self, username, password, host='localhost', 
           port=Server.DEFAULT_PORT):
-        self.name = None
         self.username = username
         self.password = password
         self.host = host
         self.port = port
         self.factory = pb.PBClientFactory()
-        print self
+        self.user = None
+        self.anteroom = None
+        print "Called Client init:", self
 
     def perspective_ping(self, arg):
         print "perspective_ping(", arg, ") called on", self
         return True
 
-    def perspective_setname(self, name):
-        print "perspective_setname(", name, ")"
-        self.name = name
-
     def __str__(self):
-        return "Client " + str(self.name)
+        return "Client " + str(self.username)
 
     def connect(self):
+        print "Client.connect", self
         user_pass = credentials.UsernamePassword(self.username, self.password)
         reactor.connectTCP(self.host, self.port, self.factory)
-        return self.factory.login(user_pass)
+        def1 = self.factory.login(user_pass, self)
+        def1.addCallbacks(self.connected, self.failure)
+        return def1
+
+    def connected(self, user):
+        print "Client.connected", self, user
+        self.user = user
+        self.anteroom = Anteroom.Anteroom(user)
+
+    def failure(self, error):
+        "Client.failure", self, error
+        reactor.stop()
+
