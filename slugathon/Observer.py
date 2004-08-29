f939d7eb0539dce702782f17c5ef4558eca6f2e6
David Ripton <dripton@ripton.net>
2004-08-29 00:54:59 +0000

    Note that observer argument may be None.

--- slugathon/Observer.py
+++ slugathon/Observer.py
@@ -3,4 +3,8 @@ import zope.interface
 class IObserver(zope.interface.Interface):
 
     def update(observed, action):
-        """Inform this observer than action has happened to observed."""
+        """Inform this observer than action has happened to observed.
+        
+           observed may be None, in which case the action should contain
+           all necessary information.
+        """
