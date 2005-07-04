3be14b333e2c22df3b5b1ab6b3c9fe1b80e42921
David Ripton <dripton@ripton.net>
2005-07-04 18:52:50 +0000

    Destroy the dialog after we get a response.

--- slugathon/SplitLegion.py
+++ slugathon/SplitLegion.py
@@ -89,6 +89,7 @@ class SplitLegion(object):
 
     def cb_response(self, widget, response_id):
         print "SplitLegion.cb_response", widget, response_id
+        self.split_legion_dialog.destroy()
         self.callback(self.old_legion, self.new_legion1, self.new_legion2)
 
 
