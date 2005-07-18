ad5ce03b668695d8a2bc8ff80e078a6d0a0c79e6
David Ripton <dripton@ripton.net>
2005-07-18 03:17:35 +0000

    Static recruiting tree data.

---
+++ slugathon/recruitdata.py
@@ -0,0 +1,53 @@
+"""Static recruiting data
+
+Terrain type to list of tuples, (creaturename, num_to_recruit)
+
+Lowercase creaturenames are special.  "anything" means anything.
+"creature" means anything with character_type of "creature"
+
+Any non-special creaturename with num_to_recruit >= 1 can be 
+recruited with one of the same type of creature, >= 1 of any
+creature later in the list, or num_to_recruit of the preceding
+creaturename.
+
+An empty tuple in the list is a break.  Neither up- or 
+down-recruiting can cross a break.
+"""
+
+# TODO First-class functions rather than magic constants?
+ANYTHING = "anything"
+CREATURE = "creature"
+
+data = {
+    "Brush": [("Gargoyle", 1), ("Cyclops", 2), ("Gorgon", 2)],
+
+    "Desert": [("Lion", 1), ("Griffon", 3), ("Hydra", 2)],
+
+    "Hills": [("Ogre", 1), ("Minotaur", 2), ("Unicorn", 2)],
+
+    "Jungle": [("Gargoyle", 1), ("Cyclops", 2), ("Behemoth", 3), 
+      ("Serpent", 2)],
+
+    "Marsh": [("Ogre", 1), ("Troll", 2), ("Ranger", 2)],
+
+    "Mountains": [("Lion", 1), ("Minotaur", 2), ("Dragon", 2), 
+      ("Colossus", 2)],
+
+    "Plains": [("Centaur", 1), ("Lion", 2), ("Ranger", 2)],
+
+    "Swamp": [("Troll", 1), ("Wyvern", 3), ("Hydra", 2)],
+
+    "Tower": [(ANYTHING, 0), ("Centaur", 1),
+              (),
+              (ANYTHING, 0), ("Gargoyle", 1),
+              (),
+              (ANYTHING, 0), ("Ogre", 1),
+              (),
+              (CREATURE, 0), ("Guardian", 3),
+              (),
+              ("Titan", 0), ("Warlock", 1)],
+
+    "Tundra": [("Troll", 1), ("Warbear", 2), ("Giant", 2), ("Colossus", 2)],
+
+    "Woods": [("Centaur", 1), ("Warbear", 2), ("Unicorn", 2)],
+}
