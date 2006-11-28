0f58e33ce5443c0bec743a7e4f78a0e5de79ace3
David Ripton <dripton@ripton.net>
2006-11-28 11:08:56 +0000

    Remove the OK button from the PickRecruit dialog.

--- slugathon/PickRecruit.py
+++ slugathon/PickRecruit.py
@@ -41,7 +41,6 @@ class PickRecruit(object):
         self.player = player
         self.legion = legion
         self.masterhex = masterhex
-        self.recruit = None
 
         self.marker = Marker.Marker(legion, scale=20)
         self.marker_hbox.pack_start(self.marker.image, expand=False,
@@ -63,9 +62,6 @@ class PickRecruit(object):
               fill=False)
             chit.connect("button_press_event", self.cb_click)
 
-        self.okbutton = self.pick_recruit_dialog.action_area.get_children()[0]
-        self.okbutton.set_sensitive(False)
-
         self.pick_recruit_dialog.connect("response", self.cb_response)
         self.pick_recruit_dialog.show()
 
@@ -75,24 +71,23 @@ class PickRecruit(object):
         eventbox = widget
         if eventbox in self.recruits_hbox.get_children():
             chit = eventbox.chit
-            self.recruit = chit.creature
-            self.okbutton.set_sensitive(True)
-        else:
-            self.recruit = None
-            self.okbutton.set_sensitive(False)
+            self.callback(self.legion, chit.creature)
+            self.pick_recruit_dialog.destroy()
 
     def cb_response(self, dialog, response_id):
-        """Send the recruit to the callback function."""
+        """The cancel button was pressed, so exit"""
         print ("PickRecruit.cb_response", dialog, response_id, 
-          type(response_id), self.recruit)
-        if response_id == gtk.RESPONSE_OK:
-            self.callback(self.legion, self.recruit)
+          type(response_id))
         self.pick_recruit_dialog.destroy()
 
 
 if __name__ == "__main__":
     creature_names = ["Titan", "Dragon", "Dragon", "Minotaur", "Minotaur"]
     creatures = Creature.n2c(creature_names)
+
+    def callback(legion, creature):
+        print legion, "recruited", creature
+        guiutils.die
    
     now = time.time()
     username = "p0"
@@ -103,7 +98,7 @@ if __name__ == "__main__":
     legion.hexlabel = 1000
     masterhex = game.board.hexes[legion.hexlabel]
     pickrecruit = PickRecruit(username, player, legion, masterhex, 
-      game.caretaker, guiutils.die, None)
+      game.caretaker, callback, None)
     pickrecruit.pick_recruit_dialog.connect("destroy", guiutils.die)
 
     gtk.main()
