6e2409067dd2ecfe73ec3b68dd0b548e0caf4506
David Ripton <dripton@ripton.net>
2007-10-11 00:16:17 +0000

    Use implements() instead of __implements__ = Rename avatarId to avatar_id

--- slugathon/Realm.py
+++ slugathon/Realm.py
@@ -1,18 +1,20 @@
 from twisted.cred import portal
 from twisted.spread import pb
+from zope.interface import implements
+
 import User
 
 class Realm(object):
-    __implements__ = portal.IRealm
+    implements(portal.IRealm)
 
     def __init__(self, server):
         print "called Realm.__init__", self, server
         self.server = server
 
-    def requestAvatar(self, avatarId, mind, *interfaces):
-        print "Called Realm.requestAvatar", self, avatarId, mind, interfaces
+    def requestAvatar(self, avatar_id, mind, *interfaces):
+        print "Called Realm.requestAvatar", self, avatar_id, mind, interfaces
         assert pb.IPerspective in interfaces
-        avatar = User.User(avatarId, self.server, mind)
+        avatar = User.User(avatar_id, self.server, mind)
         avatar.attached(mind)
         return pb.IPerspective, avatar, avatar.logout
 
