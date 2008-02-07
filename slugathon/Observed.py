df229a44157d7a4a218fb3611dab23ccdf308d0a
David Ripton <dripton@ripton.net>
2008-02-07 01:40:01 +0000

    Need to use items rather than iteritems, since the dict mutates during the loop.

--- slugathon/Observed.py
+++ slugathon/Observed.py
@@ -30,6 +30,6 @@ class Observed(object):
 
     def notify(self, action, names=None):
         # Create the list so it can't change size while iterating
-        for observer, name in self.observers.iteritems():
+        for observer, name in self.observers.items():
             if names is None or name in names:
                 observer.update(self, action)
