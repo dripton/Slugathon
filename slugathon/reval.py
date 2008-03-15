a834eb6c16d9e82de01a08d61eddd0a828cb876a
David Ripton <dripton@ripton.net>
2008-03-15 14:28:54 +0000

    Add a somewhat safer version of eval, for static dicts.

---
+++ slugathon/reval.py
@@ -0,0 +1,48 @@
+"""Read a string dict into a dict, without using eval.
+
+Based on a comp.lang.python post by Fredrik Lundh
+"""
+
+import cStringIO
+import tokenize
+
+def _parse(token, src):
+    if token[1] == "{":
+        out = {}
+        token = src.next()
+        while token[1] != "}":
+            key = _parse(token, src)
+            token = src.next()
+            if token[1] != ":":
+                raise SyntaxError("malformed dictionary")
+            value = _parse(src.next(), src)
+            out[key] = value
+            token = src.next()
+            if token[1] == ",":
+                token = src.next()
+        return out
+    elif token[1] == "[":
+        out = []
+        token = src.next()
+        while token[1] != "]":
+            out.append(_parse(token, src))
+            token = src.next()
+            if token[1] == ",":
+                token = src.next()
+        return out
+    elif token[0] == tokenize.STRING:
+        return token[1][1:-1].decode("string-escape")
+    elif token[0] == tokenize.NUMBER:
+        try:
+            return int(token[1], 0)
+        except ValueError:
+            return float(token[1])
+    elif token[0] == tokenize.NAME:
+        return eval(token[1])
+    else:
+        raise SyntaxError("malformed expression %s" % (str(token)))
+
+def reval(source):
+    src = cStringIO.StringIO(source).readline
+    src = tokenize.generate_tokens(src)
+    return _parse(src.next(), src)
---
+++ slugathon/test_reval.py
@@ -0,0 +1,25 @@
+"""
+Fredrik Lundh, comp.lang.python, 2005-11-22
+"""
+
+import py
+
+from reval import reval
+
+def test_reval_good():
+    assert reval("{'test':'123','hehe':['hooray',0x10]}") == \
+      {'test': '123', 'hehe': ['hooray', 16]} 
+
+def test_reval_good2():
+    dicstr = "{'markername': 'Rd01', 'entry_side': 1, 'teleport': False, \
+'playername': 'player', 'teleporting_lord': None, 'game_name': 'game',\
+'hexlabel': 1}"
+    assert reval(dicstr) == eval(dicstr)
+
+def test_reval_error():
+    try:
+        reval("{'test':'123','hehe':['hooray',0x10 ** 20 ** 30]}")
+    except SyntaxError:
+        pass
+    else:
+        py.test.fail("should have raised")
