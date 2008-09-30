da774c700373b43acf0b5818ea96b764fb9074af
David Ripton <dripton@ripton.net>
2008-09-30 03:43:14 +0000

    Remove unnecessary copies of Observed methods.

--- slugathon/Battle.py
+++ slugathon/Battle.py
@@ -10,17 +10,3 @@ class Battle(Observed):
         self.hexlabel = attacker_legion.hexlabel
         self.masterhex = self.game.board.hexes[self.hexlabel]
         self.entry_side = attacker_legion.entry_side
-
-    def add_observer(self, observer, name=""):
-        if observer not in self.observers:
-            self.observers[observer] = name
-
-    def remove_observer(self, observer):
-        if observer in self.observers:
-            del self.observers[observer]
-
-    def notify(self, action, names=None):
-        # Create the list so it can't change size while iterating
-        for observer, name in self.observers.items():
-            if names is None or name in names:
-                observer.update(self, action)
