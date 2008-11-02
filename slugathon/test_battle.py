39c9e905c8911a78cf6927dc8fa9453c2c968c11
David Ripton <dripton@ripton.net>
2008-11-02 17:13:09 +0000

    Split the initial legions differently so we can also test ogre moves.

--- slugathon/test_battle.py
+++ slugathon/test_battle.py
@@ -21,14 +21,14 @@ class TestBattle(object):
         game.assign_first_marker("p1", "Bu01")
         player0.pick_marker("Rd02")
         player0.split_legion("Rd01", "Rd02", 
-          ["Titan", "Centaur", "Centaur", "Gargoyle"],
-          ["Angel", "Ogre", "Ogre", "Gargoyle"])
+          ["Titan", "Centaur", "Ogre", "Gargoyle"],
+          ["Angel", "Centaur", "Ogre", "Gargoyle"])
         rd01 = player0.legions["Rd01"]
         rd01.move(6, False, None, 3)
         player1.pick_marker("Bu02")
         player1.split_legion("Bu01", "Bu02", 
-          ["Titan", "Centaur", "Centaur", "Gargoyle"],
-          ["Angel", "Ogre", "Ogre", "Gargoyle"])
+          ["Titan", "Centaur", "Ogre", "Gargoyle"],
+          ["Angel", "Centaur", "Ogre", "Gargoyle"])
         bu01 = player1.legions["Bu01"]
         bu01.move(6, False, None, 3)
         self.battle = Battle.Battle(game, bu01, rd01)
@@ -40,21 +40,22 @@ class TestBattle(object):
         assert self.battle.phase == Phase.MANEUVER
         assert self.battle.active_player == self.battle.defender_legion.player
 
-    def test_find_moves(self):
+    def test_find_moves1(self):
         defender = self.battle.defender_legion
         titan = defender.creatures[0]
         assert titan.name == "Titan"
         set1 = set(["D1", "E1", "F1", "C1", "D2", "E2", "F2", "B1", "C2",
           "D3", "E3", "F3", "A1", "B2", "C3", "D4", "E4", "F4"])
         assert self.battle.find_moves(titan) == set1
-        centaur1 = defender.creatures[1]
-        assert centaur1.name == "Centaur"
-        assert self.battle.find_moves(centaur1) == set1
-        centaur2 = defender.creatures[2]
-        assert centaur2.name == "Centaur"
-        assert self.battle.find_moves(centaur2) == set1
+        ogre = defender.creatures[1]
+        assert ogre.name == "Ogre"
+        set2 = set(["D1", "E1", "F1", "C1", "D2", "E2", "F2"])
+        assert self.battle.find_moves(ogre) == set2
+        centaur = defender.creatures[2]
+        assert centaur.name == "Centaur"
+        assert self.battle.find_moves(centaur) == set1
         gargoyle = defender.creatures[3]
         assert gargoyle.name == "Gargoyle"
-        set2 = set(["D1", "E1", "F1", "C1", "D2", "E2", "F2", "B1", "C2",
+        set3 = set(["D1", "E1", "F1", "C1", "D2", "E2", "F2", "B1", "C2",
           "D3", "E3", "F3"])
-        assert self.battle.find_moves(gargoyle) == set2
+        assert self.battle.find_moves(gargoyle) == set3
