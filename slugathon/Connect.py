65b28de32a7f27498ee9e5a0c2dfa23a1ba61391
David Ripton <dripton@ripton.net>
2008-11-08 21:46:03 +0000

    Do not pass executable as args[0] to getProcessValue
    (Docs are unclear, but I checked the Twisted source.)

--- slugathon/Connect.py
+++ slugathon/Connect.py
@@ -86,7 +86,7 @@ class Connect(object):
         def1.addErrback(self.connection_failed)
 
     def on_start_server_button_clicked(self, *args):
-        utils.getProcessValue("python", ["python", "Server.py"])
+        utils.getProcessValue("python", ["Server.py"])
 
     def connected(self, user):
         self.connect_window.hide()
