96387db7df6fac1aa5c7e66228f02ca0172b0b51
David Ripton <dripton@ripton.net>
2009-01-21 03:57:55 +0000

    Fix (and simplify) sliceborder

--- slugathon/sliceborder.py
+++ slugathon/sliceborder.py
@@ -8,12 +8,9 @@ __license__ = "GNU GPL v2"
 
 
 import sys
-import math
 
 import cairo
 
-# one-sixth of a circle
-PI_DIV_3 = math.pi / 3
 
 def slice_border_image(input_path, output_path, hexsides):
     """Copy only the specified hexsides from a battlehex border image into
@@ -27,53 +24,32 @@ def slice_border_image(input_path, output_path, hexsides):
     input_surface = cairo.ImageSurface.create_from_png(input_path)
     x_size = input_surface.get_width()
     y_size = input_surface.get_height()
+
+    vertexes = []
+    vertexes.append((0.25 * x_size, 0))
+    vertexes.append((0.75 * x_size, 0))
+    vertexes.append((x_size, 0.5 * y_size))
+    vertexes.append((0.75 * x_size, y_size))
+    vertexes.append((0.25 * x_size, y_size))
+    vertexes.append((0, 0.5 * y_size))
+    center = (0.5 * x_size, 0.5 * y_size)
+
     color = (0, 0, 0)
     output_surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, x_size, y_size)
-    input_cr = cairo.Context(input_surface)
     output_cr = cairo.Context(output_surface)
 
-    last = 0
-    for side in xrange(6):
-        if side in hexsides:
-            if side != 0:
-                rotation = PI_DIV_3 * (side - last)
-                input_cr.rotate(rotation)
-                output_cr.rotate(rotation)
-                last = side
-            # If the adjacent hexside does not share the same border type,
-            # we want to chop at the vertex to avoid overlap.  If it does,
-            # we continue to the edge to get a clean corner.
+    for hexside in hexsides:
+        output_cr.move_to(*center)
+        output_cr.line_to(*vertexes[hexside])
+        output_cr.line_to(*vertexes[(hexside + 1) % 6])
+        output_cr.close_path()
+    output_cr.clip()
 
-            # TODO Maybe we also want to go to the edge if an adjacent
-            # hex has an adjacent border of the same type.  But that would
-            # require passing more information.
-
-            # A side has the same length as the circumradius, so the
-            # vertexes are 1/4 and 3/4 of the way across.  We fudge this a
-            # bit, because our border lines have nonzero thickness.
-            fudge = 0.03
-            if (side - 1) % 6 in hexsides:
-                x0 = 0
-            else:
-                x0 = int(round(x_size * (0.25 + fudge)))
-            if (side + 1) % 6 in hexsides:
-                x1 = x_size
-            else:
-                x1 = int(round(x_size * (0.75 - fudge)))
-            height = y_size / 2
-            box = (x0, 0, x1 - x0, height)
-            input_cr.save()
-            input_cr.rectangle(*box)
-            input_cr.clip()
-            output_cr.move_to(x0, 0)
-            output_cr.set_source_surface(input_surface)
-            output_cr.fill()
-            input_cr.restore()
-    # Rotate back to original orientation
-    if last != 0:
-        rotation = -PI_DIV_3 * last
-        output_cr.rotate(rotation)
+    output_cr.move_to(0, 0)
+    output_cr.set_source_surface(input_surface)
+    output_cr.paint()
     output_surface.write_to_png(output_path)
+
 
 def main():
     assert len(sys.argv) >= 4
