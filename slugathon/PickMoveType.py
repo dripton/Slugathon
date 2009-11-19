31d449927e92c0fd37b9d9fa72c62a2fcbdc3aee
David Ripton <dripton@ripton.net>
2009-11-18 19:30:16 -0500

    Choose whether to teleport.  Not hooked up yet.  Refs #63.

---
+++ slugathon/PickMoveType.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+__copyright__ = "Copyright (c) 2005-2009 David Ripton"
+__license__ = "GNU GPL v2"
+
+
+import time
+
+import gtk
+
+import Chit
+import Marker
+import Creature
+import Legion
+import Player
+import icon
+import guiutils
+import Game
+
+
+NORMAL_MOVE = 0
+TELEPORT = 1
+
+
+class PickMoveType(object):
+    """Dialog to choose whether to teleport."""
+    def __init__(self, username, player, legion, hexlabel, callback, parent):
+        self.callback = callback
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("../ui/pickmovetype.ui")
+        self.widget_names = ["pick_move_type_dialog", "marker_hbox",
+          "chits_hbox", "legion_name", "teleport_button", "normal_move_button",
+          "cancel_button"]
+        for widget_name in self.widget_names:
+            setattr(self, widget_name, self.builder.get_object(widget_name))
+
+        self.pick_move_type_dialog.set_icon(icon.pixbuf)
+        self.pick_move_type_dialog.set_title("PickMoveType - %s" % (username))
+        self.pick_move_type_dialog.set_transient_for(parent)
+
+        self.legion_name.set_text(
+          "Pick move type for legion %s in hex %s moving to hex %s" % (
+          legion.markername, legion.hexlabel, hexlabel))
+
+        self.player = player
+        self.legion = legion
+
+        self.marker = Marker.Marker(legion, True, scale=20)
+        self.marker_hbox.pack_start(self.marker.event_box, expand=False,
+          fill=False)
+        self.marker.show()
+
+        for creature in legion.sorted_creatures():
+            if not creature.dead:
+                chit = Chit.Chit(creature, player.color, scale=20)
+                chit.show()
+                self.chits_hbox.pack_start(chit.event_box, expand=False,
+                  fill=False)
+
+        self.pick_move_type_dialog.connect("response", self.cb_response)
+        self.pick_move_type_dialog.show()
+
+
+    def cb_response(self, dialog, response_id):
+        if response_id == TELEPORT:
+            self.callback(True)
+        elif response_id == NORMAL_MOVE:
+            self.callback(False)
+        else:
+            assert response_id == gtk.RESPONSE_CANCEL
+            self.callback(None)
+        self.pick_move_type_dialog.destroy()
+
+
+if __name__ == "__main__":
+    creature_names = ["Titan", "Dragon", "Dragon", "Minotaur", "Minotaur"]
+    creatures = Creature.n2c(creature_names)
+
+    def mycallback(teleported):
+        if teleported is None:
+            print "canceled"
+        elif teleported:
+            print "teleport"
+        else:
+            print "normal move"
+        guiutils.exit()
+
+    now = time.time()
+    username = "p0"
+    game = Game.Game("g1", "p0", now, now, 2, 6)
+    player = Player.Player(username, game, 0)
+    player.color = "Red"
+    legion = Legion.Legion(player, "Rd01", creatures, 1)
+    legion.hexlabel = 100
+    hexlabel = 101
+    masterhex = game.board.hexes[legion.hexlabel]
+    mterrain = masterhex.terrain
+    pickmovetype = PickMoveType(username, player, legion, hexlabel, mycallback,
+      None)
+    pickmovetype.pick_move_type_dialog.connect("destroy", guiutils.exit)
+
+    gtk.main()
