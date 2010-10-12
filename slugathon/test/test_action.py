67912ce4d6aa2e9165437070ff51b6921bb8d74b
David Ripton <dripton@ripton.net>
2010-10-11 19:55:37 -0500

    Add (minimal, insufficient) test_eq

--- slugathon/test/test_action.py
+++ slugathon/test/test_action.py
@@ -1,4 +1,4 @@
-__copyright__ = "Copyright (c) 2008 David Ripton"
+__copyright__ = "Copyright (c) 2008-2010 David Ripton"
 __license__ = "GNU GPL v2"
 
 
@@ -17,3 +17,16 @@ def test_fromstring():
     assert obj.teleporting_lord == None
     assert obj.game_name == "game"
     assert obj.hexlabel == 1
+
+def test_eq():
+    obj1 = Action.fromstring("MoveLegion {'markername': 'Rd01', \
+'entry_side': 1, 'teleport': False, 'playername': 'player', \
+'teleporting_lord': None, 'game_name': 'game', 'hexlabel': 1}")
+    obj2 = Action.fromstring("MoveLegion {'markername': 'Rd01', \
+'entry_side': 1, 'teleport': False, 'playername': 'player', \
+'teleporting_lord': None, 'game_name': 'game', 'hexlabel': 1}")
+    assert obj1 == obj2
+    obj3 = Action.fromstring("MoveLegion {'markername': 'Rd01', \
+'entry_side': 1, 'teleport': True, 'playername': 'player', \
+'teleporting_lord': None, 'game_name': 'game', 'hexlabel': 1}")
+    assert obj1 != obj3
