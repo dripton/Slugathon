58d74e96c39cc028096d0fc93a22dd3a6722443d
David Ripton <dripton@ripton.net>
2011-10-30 14:58:31 -0400

    Use _MEIPASS if defined, for PyInstaller

--- slugathon/util/fileutils.py
+++ slugathon/util/fileutils.py
@@ -3,12 +3,18 @@ __license__ = "GNU GPL v2"
 
 
 import os
+import sys
 
 
 def basedir(*args):
     """Return an absolute path based on the base slugathon package directory
     and the passed paths."""
-    parts = [os.path.dirname(__file__), ".."]
+    if "_MEIPASS2" in os.environ: 
+        # Running from PyInstaller --onefile
+        parts = [os.environ["_MEIPASS2"]]
+    else:
+        # Running from the Git tree or dist-packages
+        parts = [os.path.dirname(__file__), ".."]
     if args:
         parts.extend(args)
     return os.path.abspath(os.path.join(*parts))
