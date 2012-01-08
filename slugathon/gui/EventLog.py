0be0c8d90def5261fff68bb3b2898da15897ab6f
David Ripton <dripton@ripton.net>
2012-01-08 15:13:23 -0500

    Bump copyright dates.

--- slugathon/gui/EventLog.py
+++ slugathon/gui/EventLog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__copyright__ = "Copyright (c) 2010-2011 David Ripton"
+__copyright__ = "Copyright (c) 2010-2012 David Ripton"
 __license__ = "GNU GPL v2"
 
 
--- slugathon/gui/Flee.py
+++ slugathon/gui/Flee.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__copyright__ = "Copyright (c) 2006-2011 David Ripton"
+__copyright__ = "Copyright (c) 2006-2012 David Ripton"
 __license__ = "GNU GPL v2"
 
 
