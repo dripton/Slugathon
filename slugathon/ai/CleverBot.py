8c1546a05d299de0a098bd79baf7db2fe65b0dee
David Ripton <dripton@ripton.net>
2012-02-04 13:52:18 -0500

    Get rid of DimBot, and merge its useful code into CleverBot.
    I don't think I ran DimBot once since CleverBot started working,
    so it was just extra baggage to maintain.

--- slugathon/ai/CleverBot.py
+++ slugathon/ai/CleverBot.py
@@ -5,6 +5,11 @@ __license__ = "GNU GPL v2"
 """An attempt at a smarter AI."""
 
 
+# Manually bump this every time this module changes enough that we want
+# results tracking to treat it as a new AI.
+VERSION = 1
+
+
 import random
 import copy
 import time
@@ -13,8 +18,9 @@ import itertools
 import collections
 
 from twisted.python import log
+from zope.interface import implementer
 
-from slugathon.ai import DimBot
+from slugathon.ai.Bot import Bot
 from slugathon.game import Game, Creature, Phase
 
 
@@ -44,12 +50,297 @@ def best7(score_moves):
     return best_moves
 
 
-class CleverBot(DimBot.DimBot):
+@implementer(Bot)
+class CleverBot(object):
     def __init__(self, playername, time_limit):
         log.msg("CleverBot", playername, time_limit)
-        DimBot.DimBot.__init__(self, playername)
+        self.playername = playername
+        self.user = None
         self.time_limit = time_limit
         self.best_creature_moves = None
+
+    def maybe_pick_color(self, game):
+        log.msg("maybe_pick_color")
+        if game.next_playername_to_pick_color == self.playername:
+            color = random.choice(game.colors_left)
+            def1 = self.user.callRemote("pick_color", game.name, color)
+            def1.addErrback(self.failure)
+
+    def maybe_pick_first_marker(self, game, playername):
+        log.msg("maybe_pick_first_marker")
+        if playername == self.playername:
+            player = game.get_player_by_name(playername)
+            markerid = self._choose_marker(player)
+            self._pick_marker(game, self.playername, markerid)
+
+    def _pick_marker(self, game, playername, markerid):
+        log.msg("pick_marker")
+        player = game.get_player_by_name(playername)
+        if markerid is None:
+            if not player.markerid_to_legion:
+                self.maybe_pick_first_marker(game, playername)
+        else:
+            player.pick_marker(markerid)
+            if not player.markerid_to_legion:
+                def1 = self.user.callRemote("pick_first_marker", game.name,
+                  markerid)
+                def1.addErrback(self.failure)
+
+    def _choose_marker(self, player):
+        """Pick a legion marker randomly, except prefer my own markers
+        to captured ones to be less annoying."""
+        own_markerids_left = [name for name in player.markerids_left if
+          name[:2] == player.color_abbrev]
+        if own_markerids_left:
+            return random.choice(own_markerids_left)
+        else:
+            return random.choice(list(player.markerids_left))
+
+    def choose_engagement(self, game):
+        """Resolve engagements."""
+        log.msg("choose_engagement")
+        if (game.pending_summon or game.pending_reinforcement or
+          game.pending_acquire):
+            log.msg("choose_engagement bailing early summon",
+              game.pending_summon,
+              "reinforcement", game.pending_reinforcement,
+              "acquire", game.pending_acquire)
+            return
+        hexlabels = game.engagement_hexlabels
+        if hexlabels:
+            hexlabel = hexlabels.pop()
+            log.msg("calling resolve_engagement")
+            def1 = self.user.callRemote("resolve_engagement", game.name,
+              hexlabel)
+            def1.addErrback(self.failure)
+        else:
+            log.msg("CleverBot calling done_with_engagements")
+            def1 = self.user.callRemote("done_with_engagements", game.name)
+            def1.addErrback(self.failure)
+
+    def resolve_engagement(self, game, hexlabel, did_not_flee):
+        """Resolve the engagement in hexlabel.
+
+        For now only know how to flee or concede.
+        """
+        log.msg("resolve_engagement", game, hexlabel, did_not_flee)
+        attacker = None
+        defender = None
+        for legion in game.all_legions(hexlabel):
+            if legion.player == game.active_player:
+                attacker = legion
+            else:
+                defender = legion
+        if attacker:
+            log.msg("attacker", attacker, attacker.player.name)
+        if defender:
+            log.msg("defender", defender, defender.player.name)
+        if not attacker or not defender:
+            log.msg("no attacker or defender; bailing")
+            return
+        if defender.player.name == self.playername:
+            if defender.can_flee:
+                if defender.score * 1.5 < attacker.score:
+                    log.msg("fleeing")
+                    def1 = self.user.callRemote("flee", game.name,
+                      defender.markerid)
+                    def1.addErrback(self.failure)
+                else:
+                    log.msg("fighting")
+                    def1 = self.user.callRemote("fight", game.name,
+                      attacker.markerid, defender.markerid)
+                    def1.addErrback(self.failure)
+            else:
+                log.msg("fighting")
+                def1 = self.user.callRemote("fight", game.name,
+                  attacker.markerid, defender.markerid)
+                def1.addErrback(self.failure)
+        elif attacker.player.name == self.playername:
+            if defender.can_flee and not did_not_flee:
+                log.msg("waiting for defender")
+                # Wait for the defender to choose before conceding.
+                pass
+            else:
+                log.msg("fighting")
+                def1 = self.user.callRemote("fight", game.name,
+                  attacker.markerid, defender.markerid)
+                def1.addErrback(self.failure)
+        else:
+            log.msg("not my engagement")
+
+    def recruit(self, game):
+        log.msg("CleverBot.recruit")
+        if game.active_player.name != self.playername:
+            log.msg("not my turn")
+            return
+        player = game.active_player
+        for legion in player.legions:
+            if legion.moved and legion.can_recruit:
+                masterhex = game.board.hexes[legion.hexlabel]
+                caretaker = game.caretaker
+                mterrain = masterhex.terrain
+                lst = legion.available_recruits_and_recruiters(mterrain,
+                  caretaker)
+                if lst:
+                    # For now, just take the last one.
+                    tup = lst[-1]
+                    recruit = tup[0]
+                    recruiters = tup[1:]
+                    log.msg("CleverBot calling recruit_creature",
+                      legion.markerid, recruit, recruiters)
+                    def1 = self.user.callRemote("recruit_creature", game.name,
+                      legion.markerid, recruit, recruiters)
+                    def1.addErrback(self.failure)
+                    return
+        log.msg("CleverBot calling done_with_recruits")
+        def1 = self.user.callRemote("done_with_recruits", game.name)
+        def1.addErrback(self.failure)
+
+    def reinforce(self, game):
+        """Reinforce, during the REINFORCE battle phase"""
+        log.msg("CleverBot.reinforce")
+        assert game.battle_active_player.name == self.playername
+        assert game.battle_phase == Phase.REINFORCE
+        legion = game.defender_legion
+        assert legion.player.name == self.playername
+        mterrain = game.battlemap.mterrain
+        caretaker = game.caretaker
+        if game.battle_turn == 4 and legion.can_recruit:
+            lst = legion.available_recruits_and_recruiters(mterrain, caretaker)
+            if lst:
+                # For now, just take the last one.
+                tup = lst[-1]
+                recruit = tup[0]
+                recruiters = tup[1:]
+                log.msg("CleverBot calling recruit_creature", recruit)
+                def1 = self.user.callRemote("recruit_creature", game.name,
+                  legion.markerid, recruit, recruiters)
+                def1.addErrback(self.failure)
+                return
+
+        log.msg("CleverBot calling done_with_reinforcements")
+        def1 = self.user.callRemote("done_with_reinforcements", game.name)
+        def1.addErrback(self.failure)
+
+    def reinforce_after(self, game):
+        """Reinforce, after the battle"""
+        log.msg("CleverBot.reinforce_after")
+        legion = game.defender_legion
+        assert legion.player.name == self.playername
+        mterrain = game.battlemap.mterrain
+        caretaker = game.caretaker
+        if legion.can_recruit:
+            lst = legion.available_recruits_and_recruiters(mterrain, caretaker)
+            if lst:
+                # For now, just take the last one.
+                tup = lst[-1]
+                recruit = tup[0]
+                recruiters = tup[1:]
+                log.msg("CleverBot calling recruit_creature", recruit)
+                def1 = self.user.callRemote("recruit_creature", game.name,
+                  legion.markerid, recruit, recruiters)
+                def1.addErrback(self.failure)
+                return
+
+        log.msg("CleverBot calling do_not_reinforce", recruit)
+        def1 = self.user.callRemote("do_not_reinforce", game.name,
+          legion.markerid)
+        def1.addErrback(self.failure)
+
+    def summon(self, game):
+        """Summon, during the REINFORCE battle phase"""
+        log.msg("CleverBot.summon")
+        assert game.active_player.name == self.playername
+        if game.battle_phase != Phase.REINFORCE:
+            return
+        legion = game.attacker_legion
+        assert legion.player.name == self.playername
+        summonables = []
+        if (legion.can_summon and game.first_attacker_kill in
+          [game.battle_turn - 1, game.battle_turn]):
+            for legion2 in legion.player.legions:
+                if not legion2.engaged:
+                    for creature in legion2.creatures:
+                        if creature.summonable:
+                            summonables.append(creature)
+            if summonables:
+                tuples = sorted(((creature.sort_value, creature)
+                  for creature in summonables), reverse=True)
+                summonable = tuples[0][1]
+                donor = summonable.legion
+                log.msg("CleverBot calling _summon_angel", legion.markerid,
+                  donor.markerid, summonable.name)
+                def1 = self.user.callRemote("summon_angel", game.name,
+                  legion.markerid, donor.markerid, summonable.name)
+                def1.addErrback(self.failure)
+                return
+
+        log.msg("CleverBot calling do_not_summon_angel", legion.markerid)
+        def1 = self.user.callRemote("do_not_summon_angel", game.name,
+          legion.markerid)
+        def1.addErrback(self.failure)
+
+        log.msg("CleverBot calling done_with_reinforcements")
+        def1 = self.user.callRemote("done_with_reinforcements", game.name)
+        def1.addErrback(self.failure)
+
+    def summon_after(self, game):
+        """Summon, after the battle is over."""
+        log.msg("CleverBot.summon_after")
+        assert game.active_player.name == self.playername
+        legion = game.attacker_legion
+        assert legion.player.name == self.playername
+        summonables = []
+        if (legion.can_summon and game.first_attacker_kill in
+          [game.battle_turn - 1, game.battle_turn]):
+            for legion2 in legion.player.legions:
+                if not legion2.engaged:
+                    for creature in legion2.creatures:
+                        if creature.summonable:
+                            summonables.append(creature)
+            if summonables:
+                tuples = sorted(((creature.sort_value, creature)
+                  for creature in summonables), reverse=True)
+                summonable = tuples[0][1]
+                donor = summonable.legion
+                log.msg("CleverBot calling _summon_angel", legion.markerid,
+                  donor.markerid, summonable.name)
+                def1 = self.user.callRemote("summon_angel", game.name,
+                  legion.markerid, donor.markerid, summonable.name)
+                def1.addErrback(self.failure)
+                return
+
+        log.msg("CleverBot calling do_not_summon_angel", legion.markerid)
+        def1 = self.user.callRemote("do_not_summon_angel", game.name,
+          legion.markerid)
+        def1.addErrback(self.failure)
+
+    def acquire_angels(self, game, markerid, num_angels, num_archangels):
+        log.msg("CleverBot.acquire_angels", markerid, num_angels,
+          num_archangels)
+        player = game.get_player_by_name(self.playername)
+        legion = player.markerid_to_legion[markerid]
+        starting_height = len(legion)
+        acquires = 0
+        angel_names = []
+        while starting_height + acquires < 7 and num_archangels:
+            angel_names.append("Archangel")
+            num_archangels -= 1
+            acquires += 1
+        while starting_height + acquires < 7 and num_angels:
+            angel_names.append("Angel")
+            num_angels -= 1
+            acquires += 1
+        if angel_names:
+            log.msg("CleverBot calling acquire_angels", markerid, angel_names)
+            def1 = self.user.callRemote("acquire_angels", game.name,
+              markerid, angel_names)
+            def1.addErrback(self.failure)
+        else:
+            log.msg("CleverBot calling do_not_acquire_angels", markerid)
+            def1 = self.user.callRemote("do_not_acquire_angels", game.name,
+              markerid)
+            def1.addErrback(self.failure)
 
     def split(self, game):
         """Split if it's my turn."""
@@ -843,3 +1134,6 @@ class CleverBot(DimBot.DimBot):
         def1 = self.user.callRemote("carry", game.name,
           best_target.name, best_target.hexlabel, carries)
         def1.addErrback(self.failure)
+
+    def failure(self, error):
+        log.err(error)
