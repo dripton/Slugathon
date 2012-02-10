c6c6715e315f1b5941b65bdcaf396466fcac91d9
David Ripton <dripton@ripton.net>
2012-02-09 20:29:33 -0500

    Remove obsolete XXX comment.

--- slugathon/ai/AIClient.py
+++ slugathon/ai/AIClient.py
@@ -352,8 +352,6 @@ class AIClient(pb.Referenceable, Observed):
         elif isinstance(action, Action.Fight):
             game = self.name_to_game(action.game_name)
             player = game.get_player_by_name(self.playername)
-            # XXX We double check the player, in case the last battle didn't
-            # get cleaned up.  (We don't call Game._end_battle2 on the client.)
             if (game.defender_legion and game.defender_legion.player.name ==
               self.playername and action.defender_markerid in
               player.markerid_to_legion):
