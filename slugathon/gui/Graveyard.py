4d26f1405ecbd4f8f2635d8e092ff6dff4c52309
David Ripton <dripton@ripton.net>
2012-07-14 16:59:38 -0400

    Add Graveyard widget, which I forgot to check in earlier.

---
+++ slugathon/gui/Graveyard.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+__copyright__ = "Copyright (c) 2012 David Ripton"
+__license__ = "GNU GPL v2"
+
+
+import gtk
+
+from slugathon.gui import Chit
+
+
+class Graveyard(gtk.EventBox):
+    """Show a legion's dead creatures."""
+    def __init__(self, legion):
+        self.legion = legion
+        gtk.EventBox.__init__(self)
+        gtkcolor = gtk.gdk.color_parse("white")
+        self.modify_bg(gtk.STATE_NORMAL, gtkcolor)
+        self.hbox = gtk.HBox(spacing=3)
+        self.add(self.hbox)
+
+    def update_gui(self):
+        playercolor = self.legion.player.color
+        for creature in self.legion.dead_creatures:
+            chit = Chit.Chit(creature, playercolor, scale=15)
+            self.hbox.pack_start(chit.event_box, expand=False)
+        self.show_all()
